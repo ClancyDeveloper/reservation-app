e8dab30b96198c5a4a2d5810a4a2d29a3f4baf94
EduardoAntonio77 <fantauva250ml@gmail.com>
2025-09-15 19:46:06 -0300

    feat('Create app for running API aplication')

--- api/app.py
+++ api/app.py
@@ -1,3 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
 from dotenv import load_dotenv
+from flask_cors import CORS
+import os
 
+load_dotenv()
+
+app = Flask(__name__)
+
+CORS(app, origins='*')
+
+@app.route("/")
+def slash_route():
+    return jsonify({
+        "message": "reservation APP",
+        "status": 200
+    }), 200
+
+print(f"Running on 127.0.0.1")
+
+if __name__ == "__main__":
+    app.run(debug=True)
