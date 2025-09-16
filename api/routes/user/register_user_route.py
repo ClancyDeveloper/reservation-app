eded4b1df18d935dd559cf3be228f33649978047
EduardoAntonio77 <fantauva250ml@gmail.com>
2025-09-16 13:52:18 -0300

    feat(Create user register route)

---
+++ api/routes/user/register_user_route.py
@@ -0,0 +1,25 @@
+from flask import jsonify, Blueprint, request
+
+from controllers.user.register_user_controller import register_user_controller
+
+register_user_blueprint = Blueprint("register_user_blueprint", __name__)
+
+@register_user_blueprint.route("/user", methods=["POST"])
+def register_user():
+
+    data = request.json
+
+    register_user_controller(data)
+
+    if error:
+        return jsonify({
+            "status": 400,
+            "message": error
+        }), 400
+
+    return jsonify({
+        "message": "Usuario registrado com sucesso",
+        "status": 200
+    }), 200
+
+    
