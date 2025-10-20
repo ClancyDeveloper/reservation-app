ff98ba537cbcd6bb58f811075ffb9c7dd532abd9
EduardoAntonio77 <fantauva250ml@gmail.com>
2025-10-20 16:45:53 -0300

    refac('removed from the controller the codes dedicated to user authentication middlewares')

--- backend/api/controllers/user/login_user_controller.py
+++ backend/api/controllers/user/login_user_controller.py
@@ -5,10 +5,4 @@ from api.database import db
 def login_user_controller(data):
     user = User.query.filter_by(username=data['username']).first()
 
-    if not user:
-        return "Usuário não encontrado"
-
-    if not user.password == data['password']:
-        return "Senha incorreta"
-
     return None
