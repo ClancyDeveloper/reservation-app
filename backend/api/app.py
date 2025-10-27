9f7e5b0c8d9ace74bde264fc4e98d94b42ff969d
EduardoAntonio77 <fantauva250ml@gmail.com>
2025-10-26 23:50:20 -0300

    refac('Import models for create tables')

--- backend/api/app.py
+++ backend/api/app.py
@@ -2,12 +2,17 @@ from flask import Flask, jsonify
 from dotenv import load_dotenv
 from flask_cors import CORS
 from api.database import db
+from model.client_model import Client
+from model.reservation_model import Reservation
 import os
 
 # users routes
 from .routes.user.register_user_route import register_user_blueprint
 from .routes.user.get_all_users_route import get_all_users_blueprint
 from .routes.user.login_user_route import login_user_blueprint
+
+# client routes
+from .routes.client.register_client_route import register_client_blueprint
 
 from .routes.slash_route import slash_route_blueprint
 
@@ -24,6 +29,7 @@ app.register_blueprint(register_user_blueprint)
 app.register_blueprint(slash_route_blueprint)
 app.register_blueprint(get_all_users_blueprint)
 app.register_blueprint(login_user_blueprint)
+app.register_blueprint(register_client_blueprint)
 
 CORS(app, origins='*')
 
